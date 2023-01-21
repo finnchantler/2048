c633bf88e5d7b71099717a3f76f6aebc3828bb33
Finn <finnchantler@gmail.com>
2023-01-21 16:31:29 +0000

    Worked on newGame funct, created grid arrays

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-function newGame() {
+let grid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
 
+const gridPositions = [
+  [{xPos: 10, yPos: 10}, {xPos: 170, yPos: 10}, {xPos: 330, yPos: 10}, {xPos: 490, yPos: 10}],
+  [{xPos: 10, yPos: 170}, {xPos: 170, yPos: 170}, {xPos: 330, yPos: 170}, {xPos: 490, yPos: 170}],
+  [{xPos: 10, yPos: 330}, {xPos: 170, yPos: 330}, {xPos: 330, yPos: 330}, {xPos: 490, yPos: 330}],
+  [{xPos: 10, yPos: 490}, {xPos: 170, yPos: 490}, {xPos: 330, yPos: 490}, {xPos: 490, yPos: 490}]
+];
+
+function newGame() {
+  canvas.width = 650;
+  canvas.height = 650;
+  ctx.fillStyle = "#FFD7BA";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "#FFE5D9";
+
+  let gridX = 10, gridY = 10;
+
+  let gridNumber = 1;
+
+  for (const rows of grid) {
+    for (const columns of rows) {
+      ctx.fillRect(gridX, gridY, 150, 150);
+      console.log(`Current grid number ${gridNumber}\nGridX = ${gridX}\ngridY = ${gridY}`);
+      gridX += 160;
+
+      gridNumber++;
+    }
+    gridX = 10;
+    gridY += 160;
+  }
 }
+
+newGame();
